05001d453c2a4956a640148f57dee93a2d7743dd
Ash-Greninja101 <80419579+Ash-Greninja101@users.noreply.github.com>
2023-04-05 18:33:54 -0500

    Progress booking somewhat done

---
+++ scripts/booking.js
@@ -0,0 +1,3 @@
+function register() {
+    document.getElementById("theBody").innerHTML = "Thanks "+document.getElementById("firstName").value+" for registering. We will get back to you in 24 to 48 hours."
+}
